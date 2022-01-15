44612448c4d34bbb0f794016c606708d5a512bf2
omarsolar <omar@savesolarcorporation.com>
2022-01-14 20:32:55 -0500

    fixes

--- scripts/notes-app.js
+++ scripts/notes-app.js
@@ -21,7 +21,7 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
         updatedAt: timestamp
     })
     saveNotes(notes);
-    location.assign(`/edit.html#${id}`);
+    location.assign(`../public/edit.html#${id}`);
 })
 
 document.querySelector('#search-text').addEventListener('input', (e) => {
